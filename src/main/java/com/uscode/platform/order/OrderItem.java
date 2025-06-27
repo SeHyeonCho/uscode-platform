3c0df61032cdd93ca17c481364792af3a058ecc0
sehyeon <Eugene8500@gmail.com>
2025-06-27 17:16:11 +0900

    feature: repository 추가

---
+++ src/main/java/com/uscode/platform/order/OrderItem.java
@@ -0,0 +1,15 @@
+package com.uscode.platform.order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+}
